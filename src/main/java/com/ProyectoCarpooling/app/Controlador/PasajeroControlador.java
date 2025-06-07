e1bc5868957236fb4bae9195cd8378e3f1b153cb
PAULA LOZANO <pau2004ortiz@gmail.com>
2025-06-07 10:23:43 -0500

    ACTUA 1

--- src/main/java/com/ProyectoCarpooling/app/Controlador/PasajeroControlador.java
+++ src/main/java/com/ProyectoCarpooling/app/Controlador/PasajeroControlador.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dashboard-pasajero")
 public class PasajeroControlador {
 
-    @GetMapping("/dashboard")
+    @GetMapping
     public String mostrarDashboardPasajero() {
         return "dashboard-pasajero"; // Coincide con dashboard-pasajero.html en templates/
     }
